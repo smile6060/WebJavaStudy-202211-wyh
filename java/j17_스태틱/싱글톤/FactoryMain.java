b322991d6779b10d4eaf46677f1a02122d1db5d2
smile6060 <dndpgml@naver.com>
2022-11-04 21:49:34 +0900

    chapter 17

---
+++ java/j17_스태틱/싱글톤/FactoryMain.java
@@ -0,0 +1,23 @@
+package j17_스태틱.싱글톤;
+
+public class FactoryMain {
+
+    public static void main(String[] args) {
+
+
+        Factory factory1 = new Factory("1공장");
+        Factory factory2 = new Factory("2공장");
+        Factory factory3 = new Factory("3공장");
+        Factory factory4 = new Factory("4공장");
+
+        SmartPhone smartPhone1 = factory1.produceSmartPhone();
+        SmartPhone smartPhone2 = factory1.produceSmartPhone();
+        SmartPhone smartPhone3 = factory1.produceSmartPhone();
+        SmartPhone smartPhone4 = factory1.produceSmartPhone();
+
+        System.out.println(smartPhone1);
+        System.out.println(smartPhone2);
+        System.out.println(smartPhone3);
+        System.out.println(smartPhone4);
+    }
+}
---
+++ java/j17_스태틱/싱글톤/SmartPhone.java
@@ -0,0 +1,14 @@
+package j17_스태틱.싱글톤;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class SmartPhone {
+    private String company;
+    private String model;
+    private String serial;
+
+
+}
