d38c181b9768158ffb89952e159e8a3146992521
smile6060 <dndpgml@naver.com>
2022-11-08 18:50:01 +0900

    chapter 18

---
+++ java/j11_배열/j18_제네릭/와일드카드/Main.java
@@ -0,0 +1,26 @@
+package j11_배열.j18_제네릭.와일드카드;
+
+public class Main {
+
+    public AnimalData<? extends Animal> getAnimal(int flag) {
+
+        if(flag == 1) {
+            AnimalData<Human> animalData = new AnimalData<Human>(new Human());
+            return animalData;
+        } else if(flag == 2) {
+            AnimalData<Tiger> animalData = new AnimalData<Tiger>(new Tiger());
+            return animalData;
+        }
+//        else if(flag == 3) {
+//            AnimalData<Car> animalData = new AnimalData<Car>(new Car("k8"));
+//            return animalData;
+//        }
+            return null;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        System.out.println(main.getAnimal(2));
+    }
+}
