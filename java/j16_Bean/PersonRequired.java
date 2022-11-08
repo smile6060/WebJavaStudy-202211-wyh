6e9882fcf226b8314da8de44491abdb255b74440
smile6060 <dndpgml@naver.com>
2022-11-08 18:49:10 +0900

    no message

--- java/j16_Bean/PersonRequired.java
+++ java/j16_Bean/PersonRequired.java
@@ -1,12 +1,12 @@
 package j16_Bean;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor //final만 가능
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class PersonRequired {
-    private final String name;
+    private final String name;              // 맴버변수에 final을 선언하면 필수값이 된다.
     private int age;
-
 }
